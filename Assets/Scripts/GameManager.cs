86cac8e006753b4b9717b2d2f6ebbd19b83a6be6
Taliayaya <ilanvinord@gmail.com>
2023-05-31 18:32:26 +0200

    Added GameManager, the brain of the game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+/// <summary>
+/// Game Manager is the main class of the game. It is a singleton that is not destroyed on load.
+/// It is the main brain of the game, and it is responsible for the game flow.
+/// </summary>
+public class GameManager : Singleton<GameManager>
+{
+    
+    void OnAwake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
