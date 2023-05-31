28d992443d1897ad5e1c0ec206f099c4154ba9df
Taliayaya <ilanvinord@gmail.com>
2023-05-31 21:48:30 +0200

    Added DataHandler.cs

---
+++ Assets/Scripts/DataHandler.cs
@@ -0,0 +1,18 @@
+﻿using ScriptableObjects.GameParameters;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public static class DataHandler
+    {
+        /// <summary>
+        /// Fetch all the game data from the resources folder and save its content
+        /// </summary>
+        public static void SaveGameData()
+        {
+            var gameParametersArray = Resources.LoadAll<GameParameters>("ScriptableObjects/Parameters");
+            foreach (var parameters in gameParametersArray)
+                parameters.SaveToFile();
+        }
+    }
+}
