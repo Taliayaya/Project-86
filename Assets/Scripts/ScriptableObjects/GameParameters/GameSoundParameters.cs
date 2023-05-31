1441ceb08e3c467d89966a995e2870698157f122
Taliayaya <ilanvinord@gmail.com>
2023-05-31 21:49:02 +0200

    Added GameParameterEditor.cs, GameParameters.cs and its variant GameSoundParameters.cs

---
+++ Assets/Scripts/ScriptableObjects/GameParameters/GameSoundParameters.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.GameParameters
+{
+    [CreateAssetMenu(fileName = "Sound Parameters", menuName = "Scriptable Objects/Game Sound Parameters", order = 1)]
+    public class GameSoundParameters : GameParameters
+    {
+        [Range(-80, -12)] public int musicVolume = -12;
+        [Range(-80, 0)] public int sfxVolume = 0;
+        
+        public override string GetParametersName => "Sound";
+    }
+}
