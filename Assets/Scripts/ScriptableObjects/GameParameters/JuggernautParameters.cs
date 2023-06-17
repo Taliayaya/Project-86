d27757e22ee51da3d0442f24d99a047fb8ab3da3
Taliayaya <ilanvinord@gmail.com>
2023-06-17 12:58:29 +0200

    JuggernautParameters.cs

---
+++ Assets/Scripts/ScriptableObjects/GameParameters/JuggernautParameters.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace ScriptableObjects.GameParameters
+{
+    [CreateAssetMenu(fileName = "JuggernautParameters", menuName = "Scriptable Objects/GameParameters/JuggernautParameters")]
+    public class JuggernautParameters : GameParameters
+    {
+        [Range(1, 100)]
+        public float mouseSensitivity = 1f;
+        [Range(10, 100)]
+        public float movementSpeed = 10f;
+        
+        public override string GetParametersName { get; } = "Juggernaut";
+    }
+}
