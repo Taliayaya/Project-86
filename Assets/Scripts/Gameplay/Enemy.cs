b6ab843e088c095bd67744fa6436f66c6a5e6d71
Taliayaya <ilanvinord@gmail.com>
2023-06-17 14:48:53 +0200

    spider orange temp jugg equivalent

---
+++ Assets/Scripts/Gameplay/Enemy.cs
@@ -0,0 +1,32 @@
+﻿using Gameplay;
+using UnityEngine;
+
+namespace DefaultNamespace.Gameplay
+{
+    public class Enemy : MonoBehaviour, IHealth
+    {
+        private float _health;
+        private float _maxHealth;
+
+        float IHealth.Health
+        {
+            get => _health;
+            set => _health = value;
+        }
+
+        float IHealth.MaxHealth
+        {
+            get => _maxHealth;
+            set => _maxHealth = value;
+        }
+
+        public void OnTakeDamage()
+        {
+        }
+
+        void IHealth.Die()
+        {
+            Destroy(gameObject);
+        }
+    }
+}
