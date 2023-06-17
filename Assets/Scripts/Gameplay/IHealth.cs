15e63001ea0285fa4d075f193304f456479fd3f1
Taliayaya <ilanvinord@gmail.com>
2023-06-17 13:01:09 +0200

    IHealth Interface

---
+++ Assets/Scripts/Gameplay/IHealth.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public interface IHealth
+    {
+        public float Health { get; set; }
+        public float MaxHealth { get; set; }
+
+        public void OnTakeDamage();
+
+        public void TakeDamage(float damageAmount)
+        {
+            Health = Mathf.Clamp(Health - damageAmount, 0, MaxHealth);
+            OnTakeDamage();
+        }
+
+        protected void Die();
+
+        public bool Alive => Health > 0;
+    }
+}
