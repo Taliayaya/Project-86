2e549a80ee419c83acbdde1519523e8a281b2020
Taliayaya <ilanvinord@gmail.com>
2023-06-22 19:30:41 +0200

    created AIAgent.cs and AgentSO.cs

---
+++ Assets/Scripts/Gameplay/Units/AIAgent.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections;
+using AI.BehaviourTree;
+using Gameplay.Mecha;
+using ScriptableObjects.AI;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Gameplay.Units
+{
+    [Serializable]
+    internal class DebugAgent
+    {
+        public bool debug = true;
+        public bool debugSelected = true;
+
+        [Header("Settings")] public bool debugSpot = true;
+        
+        [Header("Colors")]
+        public Color frontViewColor = Color.blue;
+        public Color sideViewColor = Color.cyan;
+    }
+    
+    [RequireComponent(typeof(NavMeshAgent), typeof(BehaviourTreeRunner))]
+    public class AIAgent : Unit
+    {
+        private NavMeshAgent _agent;
+        private BehaviourTreeRunner _behaviourTreeRunner;
+
+        [SerializeField] private AgentSO agentSo;
+
+        [SerializeField] private DebugAgent debugAgent;
+        private WeaponModule[] _weaponModules;
+        public BehaviourTree Tree => _behaviourTreeRunner.tree;
+        public DemoParameters demoParameters;
+
+        public override void Awake()
+        {
+            base.Awake();
+            _agent = GetComponent<NavMeshAgent>();
+            _behaviourTreeRunner = GetComponent<BehaviourTreeRunner>();
+            _weaponModules = GetComponentsInChildren<WeaponModule>();
+            if (name.Contains("Lowe"))
+            {
+                Health = demoParameters.loweHealth;
+                MaxHealth = demoParameters.loweHealth;
+            }
+            else
+            {
+                Health = demoParameters.ameiseHealth;
+                MaxHealth = demoParameters.ameiseHealth;
+            }
+
+
+        }
+
+        private void Start()
+        {
+            Tree.blackBoard.SetValue("navMeshAgent", _agent);
+            Tree.blackBoard.SetValue("transform", transform);
+            Tree.blackBoard.SetValue("agentSO", agentSo);
+            Tree.blackBoard.SetValue("weaponModules", _weaponModules);
+            Tree.blackBoard.SetValue("aiAgent", this);
+        }
+
+
+        #region AI Coroutines
+
+        
+
+        [HideInInspector] public bool isRotating;
+        public void RotateTowardsEnemy(Transform closestTarget)
+        {
+            isRotating = true;
+            StartCoroutine(RotateTowardsEnemyCoroutine(closestTarget));
+        }
+
+        public void StopRotating(Transform closestTarget)
+        {
+            StopCoroutine(RotateTowardsEnemyCoroutine(closestTarget));
+            isRotating = false;
+        }
+        
+        IEnumerator RotateTowardsEnemyCoroutine(Transform closestTarget)
+        {
+            while (true)
+            {
+                if (!closestTarget)
+                    yield break;
+                var direction = (closestTarget.position - transform.position).normalized;
+                var lookRotation = Quaternion.LookRotation(direction);
+                transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, 1);
+                yield return null;
+            }
+        }
+
+        public void StartMaintainIdealDistance(Transform closestTarget)
+        {
+            StartCoroutine(MaintainIdealDistanceCoroutine(closestTarget));
+        }
+        public void StopMaintainIdealDistance(Transform closestTarget)
+        {
+            StopCoroutine(MaintainIdealDistanceCoroutine(closestTarget));
+        }
+        IEnumerator MaintainIdealDistanceCoroutine(Transform closestTarget)
+        {
+            while (true)
+            {
+                if (!closestTarget)
+                    yield break;
+                if (agentSo.idealDistanceFromEnemy > 0)
+                {
+                    float distance = Vector3.Distance(transform.position, closestTarget.position);
+                    if (distance > agentSo.idealDistanceFromEnemy + 3)
+                    {
+                        _agent.SetDestination(closestTarget.position);
+                    }
+                    else if (distance < agentSo.idealDistanceFromEnemy - 3)
+                    {
+                        if (NavMesh.SamplePosition(transform.position - transform.forward * 5, out NavMeshHit hit, 10,
+                                NavMesh.AllAreas))
+                            _agent.SetDestination(hit.position);
+                    }
+                }
+
+                yield return new WaitForSeconds(0.5f);
+
+            }
+
+        }
+
+        #endregion
+
+        #region Debug
+
+        private void OnDrawGizmos()
+        {
+            //if (Application.isPlaying)
+            if (!debugAgent.debug)
+                return;
+            if (debugAgent.debugSelected)
+                return;
+            DebugGizmos();
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            if (!debugAgent.debug)
+                return;
+            if (!debugAgent.debugSelected)
+                return;
+            DebugGizmos();
+        }
+
+        private void DebugGizmos()
+        {
+            if (debugAgent.debugSpot)
+                DebugGizmosSpot();
+        }
+
+        private void DebugGizmosSpot()
+        {
+            var frontPoint = transform.position + transform.forward * agentSo.viewDistance;
+            var rightPoint = transform.position + Quaternion.AngleAxis(agentSo.fieldOfViewAngle / 2, Vector3.up) *
+                transform.forward * agentSo.viewDistance;
+            var leftPoint = transform.position + Quaternion.AngleAxis(-agentSo.fieldOfViewAngle / 2, Vector3.up) *
+                transform.forward * agentSo.viewDistance;
+            Gizmos.color = debugAgent.frontViewColor;
+            Gizmos.DrawLine(transform.position, frontPoint);
+            
+            Gizmos.color = debugAgent.sideViewColor;
+            Gizmos.DrawLine(transform.position, rightPoint);
+            Gizmos.DrawLine(transform.position, leftPoint);
+            
+            Gizmos.DrawLine(rightPoint, frontPoint);
+            Gizmos.DrawLine(leftPoint, frontPoint);
+
+        }
+        
+        #endregion
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/AI/AgentSO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace ScriptableObjects.AI
+{
+    // ReSharper disable once InconsistentNaming
+    [CreateAssetMenu(fileName = "Agent", menuName = "Scriptable Objects/AI/Agent")]
+    public class AgentSO : ScriptableObject
+    {
+        [Range(0, 360)] public float fieldOfViewAngle = 180;
+        [Range(0, 10000)] public float viewDistance = 10;
+        public float rotationSpeed = 1f;
+        [Range(0, 10000)] public float idealDistanceFromEnemy = 40;
+
+    }
+}
