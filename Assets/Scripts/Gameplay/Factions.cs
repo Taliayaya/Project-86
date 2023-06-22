eb4ca4acac90ae28bbb5e9adf7e1c9ee36c0e2f9
Taliayaya <ilanvinord@gmail.com>
2023-06-22 19:32:16 +0200

    Global class to access units relative to a faction

---
+++ Assets/Scripts/Gameplay/Factions.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public enum Faction
+    {
+        Republic,
+        Legion,
+    }
+    public static class Factions
+    {
+        private static readonly Dictionary<Faction, List<GameObject>> _factionMembers = new Dictionary<Faction, List<GameObject>>();
+        
+        public static void AddMember(Faction faction, GameObject member)
+        {
+            if (!_factionMembers.ContainsKey(faction))
+            {
+                _factionMembers[faction] = new List<GameObject>();
+            }
+            _factionMembers[faction].Add(member);
+        }
+        
+        public static void RemoveMember(Faction faction, GameObject member)
+        {
+            if (!_factionMembers.ContainsKey(faction))
+            {
+                return;
+            }
+            _factionMembers[faction].Remove(member);
+        }
+        
+        public static List<GameObject> GetMembers(Faction faction)
+        {
+            if (!_factionMembers.ContainsKey(faction))
+            {
+                return new List<GameObject>();
+            }
+            return _factionMembers[faction];
+        }
+
+    }
+}
