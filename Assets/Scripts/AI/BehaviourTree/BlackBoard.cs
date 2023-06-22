d3720951853f65677d7a79e9db317390a6800964
Taliayaya <ilanvinord@gmail.com>
2023-06-22 19:31:55 +0200

    now uses a dictionnary to store dynamically variable

--- Assets/Scripts/AI/BehaviourTree/BlackBoard.cs
+++ Assets/Scripts/AI/BehaviourTree/BlackBoard.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace AI.BehaviourTree
@@ -6,7 +7,64 @@ namespace AI.BehaviourTree
     [Serializable]
     public class BlackBoard
     {
-        public Vector3 moveToPosition;
-        public GameObject moveToObject;
+        private Dictionary<string, object> _values = new Dictionary<string, object>();
+        
+        public void SetValue<T>(string key, T value)
+        {
+            _values[key] = value;
+        }
+        public void SetValue(string key, object value)
+        {
+            _values[key] = value;
+        }
+        
+        public T GetValue<T>(string key)
+        {
+            return (T)_values[key];
+        }
+        public object GetValue(string key)
+        {
+            return _values[key];
+        }
+        
+        public void RemoveValue(string key)
+        {
+            _values.Remove(key);
+        }
+        
+        public bool HasValue(string key)
+        {
+            return _values.ContainsKey(key);
+        }
+        
+        public void Clear()
+        {
+            _values.Clear();
+        }
+        
+        public BlackBoard Clone()
+        {
+            var clone = new BlackBoard();
+            foreach (var pair in _values)
+            {
+                clone.SetValue(pair.Key, pair.Value);
+            }
+            return clone;
+        }
+        
+        public object Pop(string key)
+        {
+            var value = GetValue(key);
+            RemoveValue(key);
+            return value;
+        }
+        
+        public T Pop<T>(string key)
+        {
+            var value = GetValue<T>(key);
+            RemoveValue(key);
+            return value;
+        }
+            
     }
 }
