4d2b8af3a6721f4de8a589c2da4747a86a755f8d
Taliayaya <ilanvinord@gmail.com>
2023-07-07 22:36:07 +0200

    added KillerGround.cs

---
+++ Assets/Scripts/Gameplay/KillerGround.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gameplay
+{
+    [RequireComponent(typeof(Collider))]
+    public class KillerGround : MonoBehaviour
+    {
+        public float damage = 86000f;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.gameObject.TryGetComponent(out IHealth health))
+            {
+                health.TakeDamage(new DamagePackage()
+                {
+                    DamageAmount = damage,
+                    DamageSourcePosition = transform.position,
+                    Faction = Faction.Neutral
+                });
+            }
+        }
+    }
+}
