f5979fc858765a384cf432c951e19a00c754d56f
Taliayaya <ilanvinord@gmail.com>
2023-07-12 22:31:07 +0200

    tutorial SO and assets

---
+++ Assets/Scripts/ScriptableObjects/Tutorial/TutorialSO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ScriptableObjects.Tutorial
+{
+    [Serializable]
+    public struct Tutorial
+    {
+        public string title;
+        [TextArea(1, 6)]
+        public string description;
+        public Sprite image;
+    }
+    [CreateAssetMenu(fileName = "Tutorial", menuName = "Scriptable Objects/Tutorial")]
+    public class TutorialSO : ScriptableObject
+    {
+        public List<Tutorial> tutorials = new List<Tutorial>(3);
+    }
+}
