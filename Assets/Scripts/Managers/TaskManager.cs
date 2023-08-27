dfc2dc19c7a928dc5ef51f3ae58fcdee553a63e0
nemodesm <nemodesm@gmail.com>
2023-08-27 19:51:20 +0200

    Fixed indentation

--- Assets/Scripts/Managers/TaskManager.cs
+++ Assets/Scripts/Managers/TaskManager.cs
@@ -3,76 +3,76 @@ using UnityEngine;
 
 namespace Managers
 {
-	internal class TaskManager : Singleton<TaskManager>
-	{
-		public class TaskState
-		{
-			public bool Running => running;
+    internal class TaskManager : Singleton<TaskManager>
+    {
+        public class TaskState
+        {
+            public bool Running => running;
 
-			public bool Paused => paused;
+            public bool Paused => paused;
 
-			public delegate void FinishedHandler(bool manual);
-			public event FinishedHandler Finished;
+            public delegate void FinishedHandler(bool manual);
+            public event FinishedHandler Finished;
 
-			internal IEnumerator coroutine;
-			private bool running;
-			private bool paused;
-			private bool stopped;
-		
-			public TaskState(IEnumerator c)
-			{
-				coroutine = c;
-			}
-		
-			public void Pause()
-			{
-				paused = true;
-			}
-		
-			public void Unpause()
-			{
-				paused = false;
-			}
-		
-			public void Start()
-			{
-				running = true;
-				Instance.StartCoroutine(CallWrapper());
-			}
-		
-			public void Stop()
-			{
-				stopped = true;
-				running = false;
-			}
+            internal IEnumerator coroutine;
+            private bool running;
+            private bool paused;
+            private bool stopped;
+        
+            public TaskState(IEnumerator c)
+            {
+                coroutine = c;
+            }
+        
+            public void Pause()
+            {
+                paused = true;
+            }
+        
+            public void Unpause()
+            {
+                paused = false;
+            }
+        
+            public void Start()
+            {
+                running = true;
+                Instance.StartCoroutine(CallWrapper());
+            }
+        
+            public void Stop()
+            {
+                stopped = true;
+                running = false;
+            }
 
-			private IEnumerator CallWrapper()
-			{
-				yield return null;
-				var e = coroutine;
-				while(running) {
-					if(paused)
-						yield return null;
-					else {
-						if(e != null && e.MoveNext()) {
-							yield return e.Current;
-						}
-						else {
-							running = false;
-						}
-					}
-				}
-			
-				var handler = Finished;
-				handler?.Invoke(stopped);
-			}
-		}
+            private IEnumerator CallWrapper()
+            {
+                yield return null;
+                var e = coroutine;
+                while(running) {
+                    if(paused)
+                        yield return null;
+                    else {
+                        if(e != null && e.MoveNext()) {
+                            yield return e.Current;
+                        }
+                        else {
+                            running = false;
+                        }
+                    }
+                }
+            
+                var handler = Finished;
+                handler?.Invoke(stopped);
+            }
+        }
 
-		public static TaskState CreateTask(IEnumerator coroutine)
-		{
-		       if (!Instance)
-		              Debug.Log("Creating instance...)
-			return new TaskState(coroutine);
-		}
-	}
+        public static TaskState CreateTask(IEnumerator coroutine)
+        {
+            if (!Instance)
+                    Debug.Log("Creating instance...");
+            return new TaskState(coroutine);
+        }
+    }
 }
