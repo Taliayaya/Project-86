55b5ff7e3b53ab633e8692e955c84382415bc5d2
nemodesm <nemodesm@gmail.com>
2023-08-27 15:33:30 +0200

    Added Discord integration

---
+++ Assets/Scripts/Constants.cs
@@ -0,0 +1,48 @@
+public static class Constants
+{
+    /// <summary>
+    /// All the untyped event names
+    /// </summary>
+    /// <remarks>
+    /// All these event names were pulled straight from the list generated, so I can not comment on all these
+    /// </remarks>
+    public static class Events
+    {
+        public const string OnPause = "OnPause";
+        public const string OnResume = "OnResume";
+        public const string OnPrimaryFire = "OnPrimaryFire";
+        public const string OnSecondaryFire = "OnSecondaryFire";
+        public const string OnCallScavenger = "OnCallScavenger";
+        public const string OnOrderScavenger = "OnOrderScavenger";
+        public const string OnOrderSubmitScavenger = "OnOrderSubmitScavenger";
+        public const string OnStopScavenger = "OnStopScavenger";
+        public const string OnReload = "OnReload";
+        /// <summary>
+        /// FIXME: This event seems to only be called in their typed counterpart
+        /// </summary>
+        public const string OnDeath = "OnDeath";
+        /// <summary>
+        /// FIXME: This event seems to only be called in their typed counterpart
+        /// </summary>
+        public const string OnRespawn = "OnRespawn";
+    }
+    /// <summary>
+    /// All the typed event names
+    /// </summary>
+    /// <remarks>
+    /// All these event names were pulled straight from the list generated, so I can not comment on all these
+    /// </remarks>
+    public static class TypedEvents
+    {
+        // TODO : Add all the typed event names cause i couldnt be bothered -nemo
+    }
+    
+    public static class Integrations
+    {
+        public static class Discord
+        {
+            public const long AppID = 2 * 0b1011 * 0x1F * 0b101111101110001100001100000110001111111101110010111;
+            public const global::Discord.ActivityType ActivityType = 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/TaskManager.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using UnityEngine;
+
+namespace Managers
+{
+	internal class TaskManager : MonoBehaviour
+	{
+		public class TaskState
+		{
+			public bool Running => running;
+
+			public bool Paused => paused;
+
+			public delegate void FinishedHandler(bool manual);
+			public event FinishedHandler Finished;
+
+			internal IEnumerator coroutine;
+			private bool running;
+			private bool paused;
+			private bool stopped;
+		
+			public TaskState(IEnumerator c)
+			{
+				coroutine = c;
+			}
+		
+			public void Pause()
+			{
+				paused = true;
+			}
+		
+			public void Unpause()
+			{
+				paused = false;
+			}
+		
+			public void Start()
+			{
+				running = true;
+				_singleton.StartCoroutine(CallWrapper());
+			}
+		
+			public void Stop()
+			{
+				stopped = true;
+				running = false;
+			}
+
+			private IEnumerator CallWrapper()
+			{
+				yield return null;
+				var e = coroutine;
+				while(running) {
+					if(paused)
+						yield return null;
+					else {
+						if(e != null && e.MoveNext()) {
+							yield return e.Current;
+						}
+						else {
+							running = false;
+						}
+					}
+				}
+			
+				var handler = Finished;
+				handler?.Invoke(stopped);
+			}
+		}
+
+		private static TaskManager _singleton;
+
+		private void Awake()
+		{
+			_singleton = this;
+		}
+
+		public static TaskState CreateTask(IEnumerator coroutine)
+		{
+			if (!_singleton)
+			{
+				var taskManager = FindObjectOfType<TaskManager>();
+				if (taskManager is not null)
+				{
+					_singleton = taskManager;
+				}
+				if(_singleton == null) {
+					var go = new GameObject("TaskManager");
+					_singleton = go.AddComponent<TaskManager>();
+				}
+			}
+			return new TaskState(coroutine);
+		}
+	}
+}
