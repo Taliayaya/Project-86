b6022f74d1eb85ee07d39ce2a385566015ac3ae4
Taliayaya <ilanvinord@gmail.com>
2024-03-10 19:14:49 +0100

    TPS and freelook camera

--- Assets/Scripts/ScriptableObjects/GameParameters/JuggernautParameters.cs
+++ Assets/Scripts/ScriptableObjects/GameParameters/JuggernautParameters.cs
@@ -25,6 +25,8 @@ namespace ScriptableObjects.GameParameters
         [DefaultValue(100), Range(10, 500)] public float maxGrappleDistance = 100f;
         [DefaultValue(20), Range(2, 60)] public float grapplePullSpeed = 20f;
         [DefaultValue(2), Range(0.1f, 10)] public float grapplingCd = 2;
+
+        public bool toggleCockpitView = true;
                 
 
 
