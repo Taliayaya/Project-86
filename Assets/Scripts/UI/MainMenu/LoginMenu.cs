395dd2552e3aa4adc1d1ce570b8ddaf6baab8985
PopeC0rn <popcornthemaster@post.cz>
2025-12-27 20:16:10 +0100

    anonymous auth bugfix

--- Assets/Scripts/UI/MainMenu/LoginMenu.cs
+++ Assets/Scripts/UI/MainMenu/LoginMenu.cs
@@ -78,6 +78,13 @@ namespace UI.MainMenu
 
         public async void OnPlayAnonymousClicked()
         {
+            if (string.IsNullOrWhiteSpace(usernameInput.text))
+            {
+                feedbackText.text = "Please enter a username.";
+                feedbackText.color = errorColor;
+                return;
+            }
+
             try
             {
                 playAnonymousButton.interactable = false;
