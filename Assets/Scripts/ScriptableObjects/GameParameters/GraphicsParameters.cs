95c6e19ee6d4c328171dedeebd8da95f2e31ce2f
PopeCorn <117516270+PopeCorn@users.noreply.github.com>
2026-01-15 22:02:41 +0100

    fix: resolves #152 (#156)
    * anonymous auth bugfix
    
    * Revert "anonymous auth bugfix"
    
    This reverts commit 395dd2552e3aa4adc1d1ce570b8ddaf6baab8985.
    
    * anon-auth bugfix
    
    error: a player is signed in but with an invalid (empty) username, the exception causes a mismatch between UI state and backend state
    
    fix: add a check for empty string so the error does not fall through
    
    * fix: exiting submenu resumes the game
    
    * fix: reloading weapons changes blades ON/OFF to ammo count
    
    * fix: reset one player's ammo upon respawn resets other players' ammo too
    
    * refix: reloading blades UI issue (supposing that blades have MaxAmmo at 0?)
    
    * refix2: reloading blades UI issue (more robust solution)
    
    * feat: select resolution
    
    * start working on more robust resolution options
    
    * fix: GraphicsParameters event listeners never get added
    
    * integrate resolution logic in the for loop
    
    * feat: resolution fully integrated into already established checks and functions
    
    * todo and code snippets for display selection
    
    * fix: system overrides saved resolution value from last time
    
    * unify resolution and quality handling in OnGameSettingsDropdownValueChanged
    
    * feat: implement displays logic in GraphicsParameters.cs, make resolution more readable
    
    * feat: display selection, bloated, needs unifying with resolution later
    
    * readability, formatting
    
    * unify list handling in AddDropdown, add a helper function
    
    * naming, readability
    
    * add modified asset

--- Assets/Scripts/ScriptableObjects/GameParameters/GraphicsParameters.cs
+++ Assets/Scripts/ScriptableObjects/GameParameters/GraphicsParameters.cs
@@ -1,6 +1,6 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering;
 
 namespace ScriptableObjects.GameParameters
 {
@@ -11,36 +11,210 @@ namespace ScriptableObjects.GameParameters
         Medium,
         High
     }
-    
-    [CreateAssetMenu(fileName = "Graphics Parameters", menuName = "Scriptable Objects/Graphics Parameters")]
+
+    # region Structs
+    // custom structs instead of built-in ones because of serializability
+
+    [System.Serializable]
+    public struct ResolutionData : IEquatable<ResolutionData>
+    {
+        public int width;
+        public int height;
+        public int refresh_rate;
+
+        public ResolutionData(int width, int height, int refresh_rate)
+        {
+            this.width = width;
+            this.height = height;
+            this.refresh_rate = refresh_rate;
+        }
+
+        public bool Equals(ResolutionData other)
+        {
+            return width == other.width && height == other.height && refresh_rate == other.refresh_rate;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is ResolutionData other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(width, height, refresh_rate);
+        }
+
+        public override string ToString()
+        {
+            return $"{width} x {height} ({refresh_rate}Hz)";
+        }
+    }
+
+
+    [System.Serializable]
+    public struct DisplayData : IEquatable<DisplayData>
+    {
+        public string name;
+        public Vector2Int position;
+        public Vector2Int resolution;
+
+        public DisplayData(string name, Vector2Int position, Vector2Int resolution)
+        {
+            this.name = name;
+            this.position = position;
+            this.resolution = resolution;
+        }
+
+        public bool Equals(DisplayData other)
+        {
+            return name == other.name && position == other.position && resolution == other.resolution;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is DisplayData other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(name, position, resolution);
+        }
+
+        public override string ToString()
+        {
+            return $"{name} ({resolution.x} x {resolution.y})";
+        }
+    }
+
+    #endregion
+
+    [CreateAssetMenu(
+        fileName = "Graphics Parameters",
+        menuName = "Scriptable Objects/Graphics Parameters"
+    )]
     public class GraphicsParameters : GameParameters
     {
         public GraphicsQuality quality = GraphicsQuality.Medium;
-        [Range(0, 100)] public int detailsDensity = 100;
+        public ResolutionData resolution;
+        public DisplayData display;
+
+        [Range(0, 100)]
+        public int detailsDensity = 100;
+
+        // use lists instead of the default supported enums because the data are dynamically generated
+        [HideInInspector]
+        public List<ResolutionData> resolutions = new();
+
+        [HideInInspector]
+        public List<DisplayData> displays = new();
+
         public override string GetParametersName => "Graphics";
 
-        private void OnEnable()
+        public void Initialize()
         {
             EventManager.AddListener("UpdateGameParameter:quality", OnUpdateGraphicsQuality);
+            EventManager.AddListener("UpdateGameParameter:resolution", OnChangeResolution);
+            EventManager.AddListener("UpdateGameParameter:display", OnChangeDisplay);
         }
 
-        private void OnDisable()
+        public void Deinitialize()
         {
             EventManager.RemoveListener("UpdateGameParameter:quality", OnUpdateGraphicsQuality);
+            EventManager.RemoveListener("UpdateGameParameter:resolution", OnChangeResolution);
+            EventManager.RemoveListener("UpdateGameParameter:display", OnChangeDisplay);
         }
 
-        private void OnUpdateGraphicsQuality(object arg0)
+        private void OnUpdateGraphicsQuality(object _)
         {
-            Debug.Log("Changing Graphics Quality to " + arg0);
-            QualitySettings.SetQualityLevel((int) quality, true);
+            Debug.Log("Changing Graphics Quality to " + quality);
+            QualitySettings.SetQualityLevel((int)quality, true);
         }
 
+        private void OnChangeResolution(object _)
+        {
+            Debug.Log("Changing resolution to " + resolution);
+            ApplyResolution(resolution);
+        }
+
+        private void OnChangeDisplay(object _)
+        {
+            Debug.Log("Changing display to " + display.name);
+            ApplyDisplay(display);
+            ApplyResolution(resolution);
+        }
+
+        #region Helper Functions
+        private void ApplyResolution(ResolutionData res)
+        {
+            Screen.SetResolution(res.width, res.height, Screen.fullScreenMode, res.refresh_rate);
+        }
+
+        private void ApplyDisplay(DisplayData disp)
+        {
+            List<DisplayInfo> available_displays = new List<DisplayInfo>();
+            Screen.GetDisplayLayout(available_displays);
+
+            // find DisplayInfo that matches DisplayData to pass to Screen.MoveMainWindowTo
+            DisplayInfo? targetDisplay = null;
+            foreach (var d in available_displays)
+            {
+                if (d.name == disp.name &&
+                    d.width == disp.resolution.x &&
+                    d.height == disp.resolution.y)
+                {
+                    targetDisplay = d;
+                    break;
+                }
+            }
+            Screen.MoveMainWindowTo(targetDisplay.Value, Vector2Int.zero);
+        }
+
+        private void BuildResolutionList()
+        {
+            resolutions.Clear();
+            foreach (var r in Screen.resolutions)
+            {
+                int refreshRate = Mathf.RoundToInt((float)r.refreshRateRatio.value);
+                var data = new ResolutionData(r.width, r.height, refreshRate);
+                if (!resolutions.Contains(data))
+                    resolutions.Add(data);
+            }
+            if (!resolutions.Contains(resolution))
+                resolution = resolutions[^1];
+        }
+
+        private void BuildDisplayList()
+        {
+            displays.Clear();
+
+            List<DisplayInfo> available_displays = new List<DisplayInfo>();
+            Screen.GetDisplayLayout(available_displays);
+
+            foreach (var d in available_displays)
+            {
+                var displayData = new DisplayData(
+                    d.name,
+                    new Vector2Int(d.workArea.x, d.workArea.y),
+                    new Vector2Int(d.width, d.height)
+                );
+
+                if (!displays.Contains(displayData))
+                    displays.Add(displayData);
+            }
+
+            if (!displays.Contains(display))
+                display = displays[0];
+        }
+
+        #endregion
         public override void LoadFromFile()
         {
             base.LoadFromFile();
-            QualitySettings.SetQualityLevel((int) quality, true);
+            BuildDisplayList();
+            BuildResolutionList();
+            ApplyDisplay(display);
+            ApplyResolution(resolution);
+            QualitySettings.SetQualityLevel((int)quality, true);
         }
-        
-        
     }
 }
