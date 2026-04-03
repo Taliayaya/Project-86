5a2bc0fd2e6c2c016ed034674e88bddb68a05600
Taliayaya <ilanvinord@gmail.com>
2026-04-03 23:14:25 +0200

    feat: basic juggernaut ai

--- Assets/Scripts/AI/LegionSquad.cs
+++ Assets/Scripts/AI/LegionSquad.cs
@@ -166,10 +166,11 @@ namespace AI
                     if (distance > squadSO.squadMergeDistance)
                         continue;
                     
-                    Debug.Log($"Mering {name} with {legion.name} at distance {distance}");
+                    Debug.Log($"Merging {name} with {legion.name} at distance {distance}");
 
                     // squad are close to each other : merge them
-                    var legionSquad = legion.GetComponent<LegionSquad>();
+                    if (!legion.TryGetComponent<LegionSquad>(out var legionSquad))
+                        continue;
                     var newLeader = MergeSquad(legionSquad);
                     newLeader.UpdateSquadPosition();
                 }
